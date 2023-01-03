e438dfccf5cd4ea307cab1524579708136ed5282
haglass <haglass@hanmail.net>
2023-01-03 12:35:44 +0900

    todo기본기능 완료

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ app.get("*", (req, res) => {
 // req:Request
 // res:Response
 
-
 // 할일등록
 app.post("/api/post/submit", (req, res) => {
   // console.log(req.body);
@@ -74,6 +73,69 @@ app.post("/api/post/list", (req, res) => {
     })
     .catch((error) => {
       console.log(error);
+      res.status(400).json({ success: false });
+    });
+});
+// 할일 의 completed를 업데이트함
+app.post("/api/post/updatetoggle", (req, res) => {
+  // console.log(req.body);
+  let temp = {
+    completed: req.body.Completed,
+  };
+
+  // mongoose문서 참조
+  Todo.updateOne({ id: req.body.id }, { $set: temp })
+    .exec()
+    .then(() => {
+      // console.log("completed 업데이트 완료");
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+/// 타이틀 업데이트
+app.post("/api/post/updatetitle", (req, res) => {
+  // console.log(req.body);
+  let temp = {
+    title: req.body.title,
+  };
+
+  // mongoose 문서참조
+  Todo.updateOne({ id: req.body.id }, { $set: temp })
+    .exec()
+    .then(() => {
+      // console.log("completed 업데이트 완료");
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ success: false });
+    });
+});
+// 할일 삭제
+app.post("/api/post/delet", (req, res) => {
+  console.log(req.body);
+  Todo.deleteOne({ id: req.body.id })
+    .exec()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ success: false });
+    });
+});
+// 전체할일 삭제
+app.post("/api/post/deleteall", (req, res) => {
+  console.log(req.body);
+  Todo.deleteMany()
+    .exec()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
       res.status(400).json({ success: false });
     });
 });
